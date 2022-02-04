53f2b32d716c8c66c0a40bfa00e5e023bb4dc363
unknown <slifer4195@gmail.com>
2022-02-03 21:10:59 -0600

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :collectbooks do 
     member do
       get :delete
     end
   end
+  root 'collectbooks#index'
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
